2cbfba121ed21d78b01bbd145d7fb085cc9f37c5
Daniel Halverson <63027888+dhalverson@users.noreply.github.com>
2022-08-15 14:46:20 -0600

    Adds getracksforhome and getauthor endpoints

--- server/src/datasources/track-api.js
+++ server/src/datasources/track-api.js
@@ -1,2 +1,24 @@
 const { RESTDataSource } = require('apollo-datasource-rest');
 
+class TrackAPI extends RESTDataSource {
+  constructor() {
+    super();
+    this.baseURL = 'https://odyssey-lift-off-rest-api.herokuapp.com/';
+  }
+
+  // GET
+  async getTracksForHome() {
+    return this.get(
+      `tracks`
+    );
+  }
+
+  // GET AUTHOR
+  async getAuthor(authorId) {
+    return this.get(
+      `author/${encodeURIComponent(authorId)}`
+    );
+  }
+}
+
+module.exports = TrackAPI;
